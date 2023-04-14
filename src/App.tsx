dd9bc9f6d8009ea0195f3239fcb62c4ae7372612
jimmy <jimmys@axis.com>
2023-04-14 10:28:36 +0200

    chore: testest

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -11,7 +9,7 @@ function App() {
       <div>
 
       </div>
-      <h1>Jimmys GITHUB PAGE, lulle!</h1>
+      <h1>Jimmys GITHUB PAGE, lull2e!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
